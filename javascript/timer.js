5d72b8e5bc25c061645fc88f9baf0c57160c4169
woojin-1234 <0104848woo@naver.com>
2021-10-21 00:04:56 +0900

    wpqkf

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,9 +1,8 @@
-const timer = document.querySelector("h2.timer");
+const timer = document.querySelector("h2#timer");
 
 function clock(){
   const date = new Date();
-  timer.innerHTML=`${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()} ${date.getHours()}:00 기준`;
+  timer.innerText=`${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()} ${date.getHours()}:00 기준`;
 }
-
 clock();
 setInterval(clock, 1000);
